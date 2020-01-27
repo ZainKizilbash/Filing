c28566e2c6919b5a3e154fc20373670d636f0e69
ZainKizilbash <56188371+ZainKizilbash@users.noreply.github.com>
2020-01-27 22:45:21 +0500

    Add files via upload

---
+++ Output and searching.vb
@@ -0,0 +1,68 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim level As String
+        Dim section As String
+        Dim idcard As Integer
+        Dim fees As Integer
+        Dim more As String
+        Dim search As Integer
+        Dim flag As Boolean
+        flag = False
+        FileOpen(1, "D:\ABCD.txt", OpenMode.Append)
+        Do
+            Console.Write("Enter ID: ")
+            idcard = Console.ReadLine
+            Console.Write("Enter class: ")
+            level = Console.ReadLine
+            Console.Write("Enter Section: ")
+            section = Console.ReadLine
+            Console.Write("Fee Amount: ")
+            fees = Console.ReadLine
+            Console.Write("Want to enter more records? Y\N : ")
+            more = Console.ReadLine
+            WriteLine(1, idcard)
+            WriteLine(1, level)
+            WriteLine(1, section)
+            WriteLine(1, fees)
+        Loop Until more = "N" Or more = "n"
+        FileClose(1)
+        level = ""
+        section = ""
+        idcard = 0
+        fees = 0
+        more = ""
+        FileOpen(1, "D:\ABCD.txt", OpenMode.Input)
+        Console.Write("search An IDCARD: ")
+        search = Console.ReadLine
+        Do
+
+            Input(1, idcard)
+            Input(1, level)
+            Input(1, section)
+            Input(1, fees)
+
+
+            If search = idcard Then
+                flag = True
+                Console.WriteLine("Id card was found")
+                Console.WriteLine(idcard)
+                Console.WriteLine(level)
+                Console.WriteLine(section)
+                Console.WriteLine(fees)
+                Exit Do
+
+
+            End If
+
+
+        Loop Until EOF(1)
+        If flag = False Then
+            Console.WriteLine("ID card wasn't found")
+        End If
+        FileClose(1)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
